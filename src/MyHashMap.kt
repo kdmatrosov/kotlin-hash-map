b5061db87b01b5964e4a819d43b2b0ac2e33f7f4
Kirill Matrosov <k.matrosov@orbita.center>
2019-03-12 18:24:53 +0300

    init

---
+++ src/MyHashMap.kt
@@ -0,0 +1,12 @@
+class MyHashMap<Key, Value> {
+
+    fun set(key: Key, value: Value) {
+
+    }
+
+    fun get(key: Key): Value {
+        // TODO implement
+    }
+
+    inner class Data(val key: Key, val value: Value)
+}
